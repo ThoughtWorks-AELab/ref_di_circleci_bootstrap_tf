0b2463f888227039ef9a67039fb66682f55acad7
Ranbir Chawla <rchawla@thoughtworks.com>
2017-08-16 20:53:38 -0500

    testing using the customer docker runner

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
     user       = "${aws_iam_user.circle.name}"
     policy_arn = "${var.bootstrap-service-account-policy-arn}"
 }
+
+output "circle_user_key" {
+  value = "${aws_iam_access_key.circle.id}"
+}
+output "circle_user_secret" {
+  value = "${aws_iam_access_key.circle.secret}"
+}
